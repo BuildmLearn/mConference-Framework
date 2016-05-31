b4cba3b36933a37c3df7c12c08a9c11608ce8178
Jai Agarwal <f2014428@pilani.bits-pilani.ac.in>
2016-05-31 23:03:10 +0530

    Changes in Splash Screen

--- source-code/app/src/main/java/org/buildmlearn/mconference/activity/Splash.java
+++ source-code/app/src/main/java/org/buildmlearn/mconference/activity/Splash.java
@@ -1,7 +1,7 @@
 package org.buildmlearn.mconference.activity;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,7 +11,7 @@ import org.buildmlearn.mconference.R;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 3000;
 
