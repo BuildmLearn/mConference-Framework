57d0f0960e41467c5a016d1bf1efee4a3132f774
Jai Agarwal <f2014428@pilani.bits-pilani.ac.in>
2016-05-31 23:05:27 +0530

    Implemented Home Activity

--- source-code/app/src/main/java/org/buildmlearn/mconference/activity/Home.java
+++ source-code/app/src/main/java/org/buildmlearn/mconference/activity/Home.java
@@ -1,15 +1,46 @@
 package org.buildmlearn.mconference.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import org.buildmlearn.mconference.R;
+import org.buildmlearn.mconference.adapters.ConferenceDetailsAdapter;
+import org.buildmlearn.mconference.conference.About;
+import org.buildmlearn.mconference.conference.Venue;
+import org.buildmlearn.mconference.conference.Register;
+import org.buildmlearn.mconference.conference.Sponsor;
 
 public class Home extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+            toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+            viewPager = (ViewPager) findViewById(R.id.viewpager);
+            setupViewPager(viewPager);
+
+            tabLayout = (TabLayout) findViewById(R.id.tabs);
+            tabLayout.setupWithViewPager(viewPager);
+        }
+
+        private void setupViewPager(ViewPager viewPager) {
+            ConferenceDetailsAdapter adapter = new ConferenceDetailsAdapter(getSupportFragmentManager());
+            adapter.addFragment(new About(), "About");
+            adapter.addFragment(new Venue(), "Venue");
+            adapter.addFragment(new Register(), "Register");
+            adapter.addFragment(new Sponsor(), "Sponsors");
+            viewPager.setOffscreenPageLimit(0);
+            viewPager.setAdapter(adapter);
+        }
     }
-}
---
+++ source-code/app/src/main/java/org/buildmlearn/mconference/adapters/ConferenceDetailsAdapter.java
@@ -0,0 +1,40 @@
+package org.buildmlearn.mconference.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jai on 31/5/16.
+ */
+public class ConferenceDetailsAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ConferenceDetailsAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
