ff79ca3be2a2f159b35bfc486cd486c404baab2e
Jai Agarwal <f2014428@pilani.bits-pilani.ac.in>
2016-05-31 23:06:35 +0530

    Implemented About Fragment and some minor bug fixes

--- source-code/app/src/main/java/org/buildmlearn/mconference/conference/About.java
+++ source-code/app/src/main/java/org/buildmlearn/mconference/conference/About.java
@@ -18,6 +18,6 @@ public class About extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_venue, container, false);
+        return inflater.inflate(R.layout.fragment_about, container, false);
      }
 }
