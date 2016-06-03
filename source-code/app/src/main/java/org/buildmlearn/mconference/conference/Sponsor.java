3ab7804c577027915e68f6b0c2adea334f96ddfd
Jai Agarwal <f2014428@pilani.bits-pilani.ac.in>
2016-06-03 17:21:04 +0530

    Implemented Sponsor Fragment

--- source-code/app/src/main/java/org/buildmlearn/mconference/conference/Sponsor.java
+++ source-code/app/src/main/java/org/buildmlearn/mconference/conference/Sponsor.java
@@ -3,11 +3,17 @@ package org.buildmlearn.mconference.conference;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.buildmlearn.mconference.R;
+import org.buildmlearn.mconference.adapters.SponsorRecyclerView;
+import org.buildmlearn.mconference.model.SponsorDetails;
+
+import java.util.ArrayList;
 
 
 public class Sponsor extends Fragment {
@@ -22,7 +28,23 @@ public class Sponsor extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sponsor, container, false);
+        View view =  inflater.inflate(R.layout.fragment_sponsor, container, false);
+
+        boolean webPageprovided = false;
+
+        RecyclerView sponsorRecyclerView = (RecyclerView) view.findViewById(R.id.sponsor_recycler_view);
+        sponsorRecyclerView.setHasFixedSize(true);
+        sponsorRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        ArrayList<SponsorDetails> dummySponsors = new ArrayList<>(4);
+        dummySponsors.add(new SponsorDetails("Sponsor #1","https://mir-s3-cdn-cf.behance.net/project_modules/disp/3fe6027858667.55e61091bc63d.jpg"));
+        dummySponsors.add(new SponsorDetails("Sponsor #2" , "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSJqpA4Kx0817SYkXx4eZKRTOKXBjZOhHIc4mIkevic2qyaqvco"));
+        dummySponsors.add(new SponsorDetails("Sponsor #3", "https://developers.google.com/open-source/gsoc/images/gsoc2016-sun-373x373.png"));
+
+        SponsorRecyclerView sponsorAdapter = new SponsorRecyclerView(dummySponsors);
+        sponsorRecyclerView.setAdapter(sponsorAdapter);
+
+        return view;
     }
 
 }
