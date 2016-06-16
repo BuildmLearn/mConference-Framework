d3bc4b3f6e2856938475e7d44f5934cada87e690
Jai Agarwal <f2014428@pilani.bits-pilani.ac.in>
2016-06-16 16:53:04 +0530

    Fixed Google Play services issue

--- source-code/app/src/main/java/org/buildmlearn/mconference/conference/Venue.java
+++ source-code/app/src/main/java/org/buildmlearn/mconference/conference/Venue.java
@@ -1,12 +1,20 @@
 package org.buildmlearn.mconference.conference;
 
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.telecom.ConnectionRequest;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -17,6 +25,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.buildmlearn.mconference.R;
+import org.buildmlearn.mconference.util.PlayServicesUnavailableDialogFragment;
+
+import java.sql.Connection;
 
 public class Venue extends Fragment {
 
@@ -28,6 +39,7 @@ public class Venue extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_venue, container,
                 false);
+        MapsInitializer.initialize(getActivity().getApplicationContext());
         mMapView = (MapView) v.findViewById(R.id.mapView);
         TextView VenueAddress = (TextView) v.findViewById(R.id.venue_detail);
         VenueAddress.setText("Venue Address");
@@ -35,32 +47,34 @@ public class Venue extends Fragment {
 
         mMapView.onResume();// needed to get the map to display immediately
 
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity().getApplicationContext());
 
         GoogleMap googleMap = mMapView.getMap();
 
         double latitude = 17.385044;
         double longitude = 78.486671;
 
-        // create marker
-        MarkerOptions marker = new MarkerOptions().position(
-                new LatLng(latitude, longitude)).title("Hello Maps");
+        if(status == ConnectionResult.SUCCESS) {
+            // create marker
+            MarkerOptions marker = new MarkerOptions().position(
+                    new LatLng(latitude, longitude)).title("Hello Maps");
 
-        // Changing marker icon
-        marker.icon(BitmapDescriptorFactory
-                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+            // Changing marker icon
+            marker.icon(BitmapDescriptorFactory
+                    .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
 
-        // adding marker
-        googleMap.addMarker(marker);
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(17.385044, 78.486671)).zoom(12).build();
-        googleMap.animateCamera(CameraUpdateFactory
-                .newCameraPosition(cameraPosition));
+            // adding marker
+            googleMap.addMarker(marker);
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(17.385044, 78.486671)).zoom(12).build();
+            googleMap.animateCamera(CameraUpdateFactory
+                    .newCameraPosition(cameraPosition));
+        }
 
+        else {
+            DialogFragment dialogFragment = new PlayServicesUnavailableDialogFragment();
+            dialogFragment.show(getActivity().getFragmentManager(), "Play Service Problem");
+        }
         // Perform any camera updates here
         return v;
     }
