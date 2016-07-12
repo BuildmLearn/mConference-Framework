858f9665c3cc9d4bc221d65462b8283447846437
Jai Agarwal <f2014428@pilani.bits-pilani.ac.in>
2016-07-13 04:23:21 +0530

    Integrated JS XML Writers and the UI + UI Changes

--- web page/js/XMLCreater.js
+++ web page/js/XMLCreater.js
@@ -19,8 +19,10 @@ function writeXML(values) {
     XML.Node("venue", values["venue"]);
 
     XML.BeginNode("time");
-    XML.Node("start", values["venue"]);
-    XML.Node("end", values["venue"]);
+    var start = new Date(values["start-date"]);
+    XML.Node("start", start.getTime().toString());
+    var end = new Date(values["end-date"]);
+    XML.Node("end", end.getTime().toString());
 	XML.EndNode();
 
 	XML.BeginNode("splashbg");
@@ -31,6 +33,42 @@ function writeXML(values) {
 	XML.Node("aboutbg", values["about-bg"]);
 	XML.Node("details", values["details"]);
 	XML.Node("reglink", values["reg-link"]);
+
+	XML.BeginNode("sponsors");
+	XML.Attrib("type", values["sponsors"]);
+	if(values["sponsors"] == 'individual') {
+		var i;
+		for (i = 1; values["sponsor-"+i+"-name"]; i++) { 
+	    	XML.BeginNode("item");
+	    	XML.Attrib("id", i.toString());
+	    	XML.Node("name", values["sponsor-"+i+"-name"]);
+			XML.Node("image", values["sponsor-"+i+"-link"]);
+			XML.EndNode();
+		}
+	} 
+	else {
+		XML.WriteString(values["sponsors-link"]);
+	}
+	XML.EndNode();
+
+	XML.BeginNode("talks");
+	for (i = 1; values["talk-"+i+"-name"]; i++) { 
+	    	XML.BeginNode("item");
+	    	XML.Attrib("id", i.toString());
+	    	XML.Node("name", values["talk-"+i+"-name"]);
+	    	XML.Node("desc", values["description-"+i]);
+
+	    	var start = new Date(values["date-"+i] + " " + values["start-time-"+i]);
+	    	XML.Node("start", start.getTime().toString());
+
+	    	var end = new Date(values["date-"+i] + " " + values["end-time-"+i]);
+	    	XML.Node("end", end.getTime().toString());
+
+	    	XML.Node("venue", values["venue-"+i]);
+			XML.Node("image", values["talk-"+i+"-img"]);
+			XML.EndNode();
+		}
+	XML.EndNode();
 
 	XML.EndNode();
 	var blob = new Blob([XML.ToString()], {type: "application/xml;charset=utf-8"});
--- web page/js/custom.js
+++ web page/js/custom.js
@@ -1,12 +1,13 @@
 $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
+        selectMonths: true,
+        selectYears: 15
       });
 
-$(document).ready(function() {
-    $('select').material_select();
-    $('#sponsors-web').hide();
+$('#sponsors-web').hide();
+$('select').material_select();
+$('.timepicker').timepicker({ 'timeFormat': 'h:i A' });
 
+$(document).ready(function() {
    	$('#splash-bg').change(function(){
    		if($('#splash-bg').val() == 'image') {
    			$('#splash-bg-value').attr('type', 'url');
@@ -45,6 +46,19 @@ $(document).ready(function() {
    	 	var newSponsor = $("<div id=\"sponsors-"+ intId +"\" class=\"row\"><div class=\"input-field col s5 offset-s2\"><input id=\"sponsor-"+ intId +"-name\" name=\"sponsor-"+ intId +"-name\" type=\"text\" class=\"validate\"><label for=\"sponsor-"+ intId +"-name\">Sponsor#"+ intId +" Name</label></div><div class=\"input-field col s5\"><input id=\"sponsor-"+ intId +"-link\" name=\"sponsor-"+ intId +"-link\" type=\"url\" class=\"validate\"><label data-error=\"URL Invalid\" for=\"sponsor-"+ intId +"-link\">Image Link</label></div></div>");
 
    	 	$("#sponsors-list").append(newSponsor);
-
    	 });
+
+      $("#add-talks-btn").click(function() {
+         var intId = $("#talks-list>div").length + 1;
+         var newTalk = $("<div id=\"talk-"+ intId +"\"><div class=\"row  valign-wrapper\"><div class=\"col s2\"><label class=\"valign center-align\" for=\"talk-"+ intId +"\">Talk #1</label></div><div class=\"input-field col s5\"><input id=\"talk-"+ intId +"-name\" name=\"talk-"+ intId +"-name\" type=\"text\" class=\"validate\"><label for=\"talk-"+ intId +"-name\">Name</label></div><div class=\"input-field col s5\"><input id=\"venue-"+ intId +"\" name=\"venue-"+ intId +"\" type=\"text\" class=\"validate\"><label for=\"venue-"+ intId +"\">Venue</label></div></div><div class=\"row\" style=\"margin-bottom:45px;\"><div class=\"input-field col s10 offset-s2\"><textarea id=\"description-"+ intId +"\" name=\"description-"+ intId +"\" class=\"materialize-textarea\"></textarea><label for=\"description-"+ intId +"\">Description</label></div></div><div class=\"row\"><div class=\"col s4 offset-s2\"><label for=\"date-"+ intId +"\">Date</label><input id=\"date-"+ intId +"\" name=\"date-"+ intId +"\" type=\"date\" class=\"datepicker\"></div><div class=\"col s3\"><label for=\"start-time-"+ intId +"\">Start Time</label><input id=\"start-time-"+ intId +"\" name=\"start-time-"+ intId +"\" type=\"text\" autocomplete=\"on\" class=\"timepicker\"></div><div class=\"col s3\"><label for=\"end-time-"+ intId +"\">End Time</label><input id=\"end-time-"+ intId +"\" name=\"end-time-"+ intId +"\" type=\"text\" autocomplete=\"on\" class=\"timepicker\"></div></div><div class=\"row\"><div class=\"input-field col s10 offset-s2\"><input id=\"talk-"+ intId +"-img\" name=\"talk-"+ intId +"-img\" type=\"url\" class=\"validate\"><label data-error=\"URL Invalid\" for=\"talk-"+ intId +"-img\">Image Link</label></div></div></div><br><br>");
+         
+         $("#talks-list").append(newTalk);
+
+         $('.datepicker').pickadate({
+            selectMonths: true,
+            selectYears: 15
+         });
+
+         $('.timepicker').timepicker({ 'timeFormat': 'h:i A' });
+      });
 });
