056e85f86ac40af5beff229de483ea41b2f62888
Titus <mail@titus-stahl.de>
2016-08-20 14:58:19 +0200

    Missing constants; unnecessary imports

--- source-code/app/src/main/java/org/buildmlearn/mconference/activity/Splash.java
+++ source-code/app/src/main/java/org/buildmlearn/mconference/activity/Splash.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 
